02770b3c25d9de4e05737be042275a9ff03489c2
matcoco <b_jayson@hotmail.com>
2020-02-27 16:15:55 +0100

    route mail + testu ok

---
+++ back/routes/image.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const connection = require("../config");
+const parser = require("body-parser");
+
+router.use(parser.json());
+
+router.post("/", (req, res) => {
+    const image = req.body;
+    console.log(image);
+    const sql = "INSERT INTO image (name, url, alt) VALUES (? , ? , ?)";
+    connection.query(
+      sql,
+      [
+        image.name,
+        image.url,
+        image.alt,
+      ],
+      (error, results, fields) => {
+        if (error) {
+          res.status(501).send("couldn't post image" + error);
+        } else {
+          req.body.id = results.insertId;
+          res.json(req.body);
+        }
+      }
+    );
+  });
+
+
+  router.get("/", (req, res) => {
+    const sql = "SELECT * FROM image";
+    connection.query(sql, (error, results, fields) => {
+      if (error) {
+        res.status(501).send("couldn't get image");
+      } else {
+        res.json(results);
+      }
+    });
+  });
+
+  router.get("/:id", (req, res) => {
+    const idImageOne = parseInt(req.params.id);
+    const sql = "SELECT * FROM image where id = ?";
+    connection.query(sql, [idImageOne], (error, results, fields) => {
+      if (error) {
+        res.status(501).send("couldn't get reference");
+      } else {
+        res.json(results);
+      }
+    });
+  });
+
+  router.put("/:id", (req, res) => {
+    const idImage = req.params.id;
+    const image = req.body;
+    const sql = `UPDATE image SET name=?, url=?, alt=? WHERE id=${idImage}`;
+    connection.query(
+      sql,
+      [
+        image.category,
+        image.type,
+        image.section,
+        idImage
+      ],
+      (error, results, fields) => {
+        if (error) {
+          res.status(501).send("couldn't put Image" + error);
+        } else {
+          res.json(req.body);
+        }
+      }
+    );
+  });
+
+  router.delete("/:id", (req, res) => {
+    const idImage = req.params.id;
+    const sql = "DELETE FROM image WHERE id=?";
+    connection.query(sql, [idImage], (error, results, fields) => {
+      if (error) {
+        res.status(501).send("couldn't put image" + error);
+      } else {
+        res.json(req.body);
+      }
+    });
+  });
+
+  module.exports = router;
--- back/routes/index.js
+++ back/routes/index.js
@@ -5,17 +5,17 @@ const router = express.Router();
 //const demonstration = require("./demonstration");
 //const file = require("./file");
 //const homepage = require("./homepage");
-//const image = require("./image");
-//const mail = require("./mail");
+const image = require("./image");
+const mail = require("./mail");
 const reference = require("./reference");
 //const solution = require("./solutions");
 
 //router.use("/admin", admin);
 //router.use("/demonstration", demonstration);
 //router.use("/file", file);
-//router.use("/image", image);
+router.use("/image", image);
 //router.use("/homepage", homepage);
-//router.use("/mail", mail);
+router.use("/mail", mail);
 router.use("/reference", reference);
 //router.use("/solution", solution);
 
