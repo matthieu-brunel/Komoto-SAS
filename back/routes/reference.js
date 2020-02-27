58a4905c89ce1132d60fdd88a89e5db57be9fb32
Alexandra Brazy <morthis1014@gmail.com>
2020-02-27 14:23:31 +0100

    routes

--- back/routes/reference.js
+++ back/routes/reference.js
@@ -33,7 +33,7 @@ router.get("/", (req, res) => {
   const sql = "SELECT * FROM reference";
   connection.query(sql, (error, results, fields) => {
     if (error) {
-      res.status(501).send("couldn't get blog");
+      res.status(501).send("couldn't get reference");
     } else {
       res.json(results);
     }
@@ -41,7 +41,7 @@ router.get("/", (req, res) => {
 });
 router.get("/:id", (req, res) => {
   const idReferenceOne = parseInt(req.params.id);
-  const sql = "SELECT * FROM reference";
+  const sql = "SELECT * FROM reference where id = ?";
   connection.query(sql, [idReferenceOne], (error, results, fields) => {
     if (error) {
       res.status(501).send("couldn't get reference");
@@ -50,5 +50,39 @@ router.get("/:id", (req, res) => {
     }
   });
 });
-
+router.put("/:id", (req, res) => {
+  const idReference = req.params.id;
+  const reference = req.body[0];
+  console.log("text", req.body);
+  const sql = `UPDATE reference SET category=?, type=?, section=?, description=?, image_id=? WHERE id=${idReference}`;
+  connection.query(
+    sql,
+    [
+      reference.category,
+      reference.type,
+      reference.section,
+      reference.description,
+      reference.image_id,
+      idReference
+    ],
+    (error, results, fields) => {
+      if (error) {
+        res.status(501).send("couldn't put reference" + error);
+      } else {
+        res.json(req.body);
+      }
+    }
+  );
+});
+router.delete("/:id", (req, res) => {
+  const idReference = req.params.id;
+  const sql = "DELETE  FROM reference WHERE id=?";
+  connection.query(sql, [idReference], (error, results, fields) => {
+    if (error) {
+      res.status(501).send("couldn't put reference" + error);
+    } else {
+      res.json(req.body);
+    }
+  });
+});
 module.exports = router;
--- back/spec/reference.spec.js
+++ back/spec/reference.spec.js
@@ -3,6 +3,10 @@ require("dotenv").config();
 
 const SERVER_ADDRESS_FULL = process.env.REACT_APP_SERVER_ADDRESS_FULL;
 
+let obj = {
+  id: null
+};
+
 describe("test reference CRUD", () => {
   let server = null;
   let data = {};
@@ -14,6 +18,33 @@ describe("test reference CRUD", () => {
     description: "test_description",
     image_id: 1
   };
+
+  it("post reference", done => {
+    request(
+      {
+        method: "post",
+        json: true,
+        url: SERVER_ADDRESS_FULL + "/api/reference",
+        headers: {},
+        body: test_reference
+      },
+      (error, response, body) => {
+        expect(response.statusCode).toBe(200);
+
+        obj.id = body.id;
+
+        data = body;
+
+        test_reference.id = body.id;
+        expect(data.category).toBe(test_reference.category);
+        expect(data.type).toBe(test_reference.type);
+        expect(data.section).toBe(test_reference.section);
+        expect(data.description).toBe(test_reference.description);
+        expect(data.image_id).toBe(test_reference.image_id);
+        done();
+      }
+    );
+  });
 
   it("get reference", done => {
     request(
@@ -29,62 +60,39 @@ describe("test reference CRUD", () => {
     );
   });
 
-  it("post reference", done => {
-    request(
-      {
-        method: "post",
-        json: true,
-        url: SERVER_ADDRESS_FULL + "/api/reference",
-        headers: {},
-        body: test_reference
-      },
-      (error, response, body) => {
-        expect(response.statusCode).toBe(200);
-        console.log("BODY", body);
-        data = body;
-        test_reference.id = body.id;
-        expect(data.category).toBe(test_reference.category);
-        expect(data.type).toBe(test_reference.type);
-        expect(data.section).toBe(test_reference.section);
-        expect(data.description).toBe(test_reference.description);
-        expect(data.image_id).toBe(test_reference.image_id);
-        done();
-      }
-    );
-  });
-  it("put reference", done => {
-    (test_reference.category = "category_test"),
-      (test_reference.type = "type_test");
-    test_reference.section = "section_test";
-    test_reference.description = "description_test";
-    test_reference.image_id = "2";
+  it("should update reference", done => {
+    test_reference.category = "new put";
+    test_reference.type = "new put";
+    test_reference.section = "new put";
+    test_reference.description = "new put";
+    test_reference.image_id = 2;
 
-    request(
+    request.put(
       {
-        method: "put",
+        url: SERVER_ADDRESS_FULL + "/api/reference/" + obj.id,
         json: true,
-        url: SERVER_ADDRESS_FULL + "/api/reference/" + test_reference.id,
         headers: {},
         body: test_reference
       },
+
       (error, response, body) => {
-        expect(response.statusCode).toBe(200);
-        data = body;
-        expect(data.category).toBe(test_reference.category);
-        expect(data.type).toBe(test_reference.type);
-        expect(data.section).toBe(test_reference.section);
-        expect(data.description).toBe(test_reference.description);
-        expect(data.image_id).toBe(test_reference.image_id);
+        console.log("PUT", body);
+        expect(body.category).toBe(test_reference.category);
+        expect(body.type).toBe(test_reference.type);
+        expect(body.section).toBe(test_reference.section);
+        expect(body.description).toBe(test_reference.description);
+        expect(body.image_id).toBe(test_reference.image_id);
         done();
       }
     );
   });
+
   it("delete reference", done => {
     request(
       {
         method: "delete",
         json: true,
-        url: SERVER_ADDRESS_FULL + "/api/reference" + test_reference.id,
+        url: SERVER_ADDRESS_FULL + "/api/reference/" + obj.id,
         headers: {}
       },
       (error, response, body) => {
