403c00a9d248bca3765f550a50b0470ef82bd59b
matcoco <b_jayson@hotmail.com>
2020-03-05 14:21:34 +0100

    fix route get sql join sur table image et homepage

---
+++ utils/getRessource.js
@@ -0,0 +1,8 @@
+//const REACT_APP_SERVER_ADDRESS_FULL = process.env.REACT_APP_SERVER_ADDRESS_FULL;
+
+export default async function getRessource(table) {
+
+    let url =  '/api/';
+
+    return url;
+}
