10e8ee408d4006a16e0321374591f9224a5de199
matcoco <b_jayson@hotmail.com>
2020-03-05 10:10:02 +0100

    test fetch

--- front/src/Components/Accueil/ReferencesAccueil/Reference.jsx
+++ front/src/Components/Accueil/ReferencesAccueil/Reference.jsx
@@ -1,12 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Reference.css';
+import { NavLink } from 'react-router-dom';
 
-function ReferenceAccueil() {
-  return (
-    <div className="">
-      
-    </div>
-  );
+import getRessources from '../../../utils/getRessources';
+
+class ReferenceAccueil extends Component {
+  constructor(){
+    super()
+    this.state = {
+      reference:[]
+    }
+}
+
+/* componentDidMount(){
+  this.getReference();
+}
+
+getReference(){
+
+  fetch(REACT_APP_SERVER_ADDRESS_FULL+"/api/homepage",
+  {
+      headers: new Headers({
+          'Content-Type': 'application/json'
+      }),
+  })
+  .then(
+  (res) => res.json()
+  )
+  .then(
+  (res) => 
+      {
+         this.setState({ reference: res }, () => {
+          })
+      }
+  )
+} */
+
+componentDidMount = async () => {
+
+  let data2 = await getRessources('homepage','reference');
+   this.setState({
+    reference:data2
+  }) 
+}
+
+  render(){
+    return (
+      <div className="">
+        {this.state.reference.map((reference, index) => {
+          return (
+            <div key={index}>
+              <NavLink  to="/Reference">référence {reference.id}</NavLink>
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
 }
 
 export default ReferenceAccueil;
--- front/src/Components/Accueil/Specialisation/Specialisation.jsx
+++ front/src/Components/Accueil/Specialisation/Specialisation.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Specialisation.css';
+import getRessources from '../../../utils/getRessources';
+import { NavLink } from 'react-router-dom';
 
-function SpecialisationAccueil() {
+class SpecialisationAccueil extends Component {
+  constructor(){
+    super()
+    this.state = {
+      specialisation:[]
+    }
+}
+
+componentDidMount = async () => {
+
+  let data2 = await getRessources('homepage','specialisation');
+   this.setState({
+    specialisation:data2
+  })
+  
+}
+
+render(){
   return (
     <div className="">
-      
+      {this.state.specialisation.map((specialisation, index) => {
+        return (
+          <div key={index}>
+             <NavLink  to="/">specialisation {specialisation.id}</NavLink>
+          </div>
+        )
+      })}
     </div>
   );
 }
+}
 
-export default SpecialisationAccueil;
+export default SpecialisationAccueil
