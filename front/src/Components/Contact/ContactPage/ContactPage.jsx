ed0fe7a4343a535aec34316752ea86fbb8671a7b
Alexandra Brazy <morthis1014@gmail.com>
2020-03-19 12:09:45 +0100

    contact

--- front/src/Components/Contact/ContactPage/ContactPage.jsx
+++ front/src/Components/Contact/ContactPage/ContactPage.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import './ContactPage.css';
+import React, { Component } from "react";
+import "./ContactPage.css";
+import getRessources from "../../../utils/getRessources";
+import { Link } from "react-router-dom";
 
+class ContactPage extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-
-function ContactPage() {
-  return (
-    <div className="">
-      
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <form action="mail" method="post">
+          <div class="border-secondary rounded-0">
+            <div class="card-header p-0">
+              <div class="bg-secondary text-white text-center py-2">
+                <h3>
+                  <p>Nous contacter</p>
+                </h3>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default ContactPage;
